4fefc330305b759219d9433cae05729d2e4ee31d
Bomb787 <39660651+Bomb787@users.noreply.github.com>
2021-03-17 22:06:32 -0400

    Added comments, deleted swords, added more weapons

--- src/main/java/bwm/core/init/ItemInit.java
+++ src/main/java/bwm/core/init/ItemInit.java
@@ -2,21 +2,31 @@ package bwm.core.init;
 
 import com.mrcrayfish.guns.item.AmmoItem;
 import com.mrcrayfish.guns.item.GunItem;
+import bwm.geo.TestGunRenderer;
 
 import bwm.WeaponsMod;
-import bwm.util.ItemTiers;
+import bwm.geo.TestGunItem;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.SwordItem;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
+/**
+ * This class is where all of the mod's items are registered
+ * Author: Bomb787
+ */
 public class ItemInit {
 	
+	/*
+     * This creates a Deferred Register where all of the items will be registered
+     * This is called and added to the event bus in the main mod file.
+     */
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS,
 			WeaponsMod.MOD_ID);
+	
+	public static final RegistryObject<TestGunItem> TEST_GUN = ITEMS.register("geotest",
+			() -> new TestGunItem(new Item.Properties().group(WeaponsMod.material).setISTER(() -> TestGunRenderer::new)));
 	
 	public static final RegistryObject<Item> ALUMINIUM_INGOT = ITEMS.register("aluminium_ingot",
 			() -> new Item(new Item.Properties().group(WeaponsMod.material)));
@@ -42,9 +52,6 @@ public class ItemInit {
 	public static final RegistryObject<Item> POLYMER_BAR = ITEMS.register("polymer_bar",
 			() -> new Item(new Item.Properties().group(WeaponsMod.material)));
 	
-	public static final RegistryObject<Item> SHARPENING_KIT = ITEMS.register("sharpening_kit",
-			() -> new Item(new Item.Properties().group(ItemGroup.MISC)));
-	
 	//Block Items
 	public static final RegistryObject<BlockItem> BAUXITE_ORE = ITEMS.register("bauxite_ore", 
 			() -> new BlockItem(BlockInit.BAUXITE_ORE.get(), new Item.Properties().group(WeaponsMod.material)));
@@ -58,37 +65,36 @@ public class ItemInit {
 	public static final RegistryObject<BlockItem> POLYMER_BLOCK = ITEMS.register("polymer_block", 
 			() -> new BlockItem(BlockInit.POLYMER_BLOCK.get(), new Item.Properties().group(WeaponsMod.material)));
 	
-	//Sword Items
-	public static final RegistryObject<SwordItem> ALUMINIUM_SWORD = ITEMS.register("aluminium_sword", 
-			() -> new SwordItem(ItemTiers.ALUMINIUM, 0, -2.4F, new Item.Properties().group(ItemGroup.COMBAT)));
-	
-	public static final RegistryObject<SwordItem> STEEL_SWORD = ITEMS.register("steel_sword", 
-			() -> new SwordItem(ItemTiers.STEEL, 0, -2.4F, new Item.Properties().group(ItemGroup.COMBAT)));
-	
-	public static final RegistryObject<SwordItem> SHARPENED_ALUMINIUM_SWORD = ITEMS.register("sharpened_aluminium_sword", 
-			() -> new SwordItem(ItemTiers.SHARPENED_ALUMINIUM, 0, -2.4F, new Item.Properties().group(ItemGroup.COMBAT)));
-	
-	public static final RegistryObject<SwordItem> SHARPENED_STEEL_SWORD = ITEMS.register("sharpened_steel_sword", 
-			() -> new SwordItem(ItemTiers.SHARPENED_STEEL, 0, -2.4F, new Item.Properties().group(ItemGroup.COMBAT)));
-	
-	public static final RegistryObject<SwordItem> SHARPENED_IRON_SWORD = ITEMS.register("sharpened_iron_sword", 
-			() -> new SwordItem(ItemTiers.SHARPENED_IRON, 0, -2.4F, new Item.Properties().group(ItemGroup.COMBAT)));
-	
-	public static final RegistryObject<SwordItem> SHARPENED_DIAMOND_SWORD = ITEMS.register("sharpened_diamond_sword", 
-			() -> new SwordItem(ItemTiers.SHARPENED_DIAMOND, 0, -2.4F, new Item.Properties().group(ItemGroup.COMBAT)));
-	
-	public static final RegistryObject<SwordItem> SHARPENED_NETHERITE_SWORD = ITEMS.register("sharpened_netherite_sword", 
-			() -> new SwordItem(ItemTiers.SHARPENED_NETHERITE, 0, -2.4F, new Item.Properties().group(ItemGroup.COMBAT)));
-	
 	//Gun Items
 	public static final RegistryObject<GunItem> BIG_IRON = ITEMS.register("big_iron", () -> new GunItem(new Item.Properties().maxStackSize(1).group(WeaponsMod.gun)));
 	
 	public static final RegistryObject<GunItem> GLOCK_17 = ITEMS.register("glock_17", () -> new GunItem(new Item.Properties().maxStackSize(1).group(WeaponsMod.gun)));
+	
+	public static final RegistryObject<GunItem> TRASHCAN = ITEMS.register("trashcan", () -> new GunItem(new Item.Properties().maxStackSize(1).group(WeaponsMod.gun)));
+	
+	public static final RegistryObject<GunItem> AK_74 = ITEMS.register("ak_74", () -> new GunItem(new Item.Properties().maxStackSize(1).group(WeaponsMod.gun)));
+	
+	public static final RegistryObject<GunItem> AK_103 = ITEMS.register("ak_103", () -> new GunItem(new Item.Properties().maxStackSize(1).group(WeaponsMod.gun)));
+	
+	public static final RegistryObject<GunItem> GAUSS_RIFLE = ITEMS.register("gauss_rifle", () -> new GunItem(new Item.Properties().maxStackSize(1).group(WeaponsMod.gun)));
+	
+	public static final RegistryObject<GunItem> HK417 = ITEMS.register("hk417", () -> new GunItem(new Item.Properties().maxStackSize(1).group(WeaponsMod.gun)));
+	
+	public static final RegistryObject<GunItem> M4A1 = ITEMS.register("m4a1", () -> new GunItem(new Item.Properties().maxStackSize(1).group(WeaponsMod.gun)));
 	
 	//Bullet Items
 	public static final RegistryObject<Item> AMMO762X39 = ITEMS.register("762x39", () -> new AmmoItem(new Item.Properties().group(WeaponsMod.bullet)));
 	
 	public static final RegistryObject<Item> AMMO9X19 = ITEMS.register("9mm_parabellum", () -> new AmmoItem(new Item.Properties().group(WeaponsMod.bullet)));
 	
+	public static final RegistryObject<Item> GARBAGE = ITEMS.register("garbage", () -> new AmmoItem(new Item.Properties().group(WeaponsMod.bullet)));
+	
+	public static final RegistryObject<Item> AMMO545X39 = ITEMS.register("545x39", () -> new AmmoItem(new Item.Properties().group(WeaponsMod.bullet)));
+	
+	public static final RegistryObject<Item> REDSTONE_BULLET = ITEMS.register("redstone_bullet", () -> new AmmoItem(new Item.Properties().group(WeaponsMod.bullet)));
+	
+	public static final RegistryObject<Item> AMMO762x51 = ITEMS.register("762x51", () -> new AmmoItem(new Item.Properties().group(WeaponsMod.bullet)));
+	
+	public static final RegistryObject<Item> AMMO556x45 = ITEMS.register("556x45", () -> new AmmoItem(new Item.Properties().group(WeaponsMod.bullet)));
 	
 }
