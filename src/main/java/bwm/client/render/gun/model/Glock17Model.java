0f950794dfcb20a8fd4870e8af4c06b7616635fd
Bomb787 <39660651+Bomb787@users.noreply.github.com>
2021-03-17 21:31:39 -0400

    Added comments, added correct animation.

--- src/main/java/bwm/client/render/gun/model/Glock17Model.java
+++ src/main/java/bwm/client/render/gun/model/Glock17Model.java
@@ -11,38 +11,52 @@ import net.minecraft.client.renderer.model.ItemCameraTransforms.TransformType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.CooldownTracker;
-import net.minecraft.util.math.vector.Vector3f;
 
+/*
+ * Since we want to have an animation for the slide of the gun, we will be overriding the standard model rendering.
+ */
 public class Glock17Model implements IOverrideModel{
 
+	@SuppressWarnings("resource")
 	@Override
 	public void render(float partialTicks, TransformType transformType, ItemStack stack, ItemStack parent, LivingEntity entity, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, int light, int overlay) {
 		
+		//Renders the static parts of the model, the magazine is separate from the base for possible future use.
 		RenderUtil.renderModel(SpecialModels.GLOCK17BASE.getModel(), stack, matrixStack, renderTypeBuffer, light, overlay);
+		RenderUtil.renderModel(SpecialModels.GLOCK17MAGAZINE.getModel(), stack, matrixStack, renderTypeBuffer, light, overlay);
 		
 		if(entity.equals(Minecraft.getInstance().player)) {
 			
-            		matrixStack.push();
-            		matrixStack.translate(0, -5.8 * 0.0625, 0);
-           		 CooldownTracker tracker = Minecraft.getInstance().player.getCooldownTracker();
-            		float cooldown = tracker.getCooldown(stack.getItem(), Minecraft.getInstance().getRenderPartialTicks());
-            		cooldown = (float) easeInOutBack(cooldown);
-            		matrixStack.rotate(Vector3f.ZN.rotationDegrees(45F * cooldown));
-            		matrixStack.translate(0, 5.8 * 0.0625, 0);
-            		RenderUtil.renderModel(SpecialModels.GLOCK17SLIDE.getModel(), stack, matrixStack, renderTypeBuffer, light, overlay);
-            		matrixStack.pop();
+			//Always push.
+            matrixStack.push();
+            //Don't touch this, it's better to use the display options in Blockbench.
+            matrixStack.translate(0, -5.8 * 0.0625, 0);
+            //Gets the cooldown tracker for the item. Items like swords and enderpearls also have this.
+            CooldownTracker tracker = Minecraft.getInstance().player.getCooldownTracker();
+            float cooldown = tracker.getCooldown(stack.getItem(), Minecraft.getInstance().getRenderPartialTicks());
+            cooldown = (float) easeOutExpo(cooldown);
+            /**
+             * We are moving whatever part is moving.
+             * X,Y,Z, use Z for moving back and forth.
+             */
+            matrixStack.translate(0, 0, cooldown/6.5);
+            matrixStack.translate(0, 5.8 * 0.0625, 0);
+            //Renders the moving part of the gun.
+            RenderUtil.renderModel(SpecialModels.GLOCK17SLIDE.getModel(), stack, matrixStack, renderTypeBuffer, light, overlay);
+            //Always pop
+            matrixStack.pop();
             
-        	}
+        }
 		
 	}
 	
-	//Temporary for testing
-	private double easeInOutBack(double x) {
+	/**
+     * Easing function based on code from https://easings.net/#easeInOutBack
+     */
+	private double easeOutExpo(double x) {
 		
-        	double c1 = 1.70158;
-        	double c2 = c1 * 1.525;
-        	return (x < 0.5 ? (Math.pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2 : (Math.pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2);
-		
-	}
+		return x == 1 ? 1 : 1 - Math.pow(2, -10 * x);
+        
+    }
 
 }
