d58154c7cb70ca80536f37c7f54c2821ca73d215
Bomb787 <39660651+Bomb787@users.noreply.github.com>
2021-03-17 21:26:36 -0400

    Added comments.

--- src/main/java/bwm/world/OreGeneration.java
+++ src/main/java/bwm/world/OreGeneration.java
@@ -13,18 +13,25 @@ import net.minecraft.world.gen.placement.TopSolidRangeConfig;
 import net.minecraftforge.common.world.BiomeGenerationSettingsBuilder;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 
+/*
+ * This class tells the mod how to generate the ores that will be added.
+ * Author: Bomb787
+ */
 public class OreGeneration {
 	
 	public static void generateOres(final BiomeLoadingEvent event) {
 		
+		//Initiates the values and gets them from our config file.
 		final boolean generateBauxite = WeaponsConfig.COMMON.ores.generateBauxite.get();
 		final int maxBauxiteVein = WeaponsConfig.COMMON.ores.maxBauxiteVein.get();
 		final int minBauxiteY = WeaponsConfig.COMMON.ores.minBauxiteY.get();
 		final int maxBauxiteY = WeaponsConfig.COMMON.ores.maxBauxiteY.get();
 		final int maxBauxiteChunk = WeaponsConfig.COMMON.ores.maxBauxiteChunk.get();
 		
+		//Makes sure that the ore only generates in the Overworld.
 		if(!(event.getCategory().equals(Biome.Category.NETHER)) || (event.getCategory().equals(Biome.Category.THEEND))) {
 			
+			//Makes sure that the config has bauxite ore enabled.
 			if(generateBauxite == true)
 			//Default max vein size of 6, minimum Y of 32, max of 56, at most 18 per chunk
 			generateOre(event.getGeneration(), OreFeatureConfig.FillerBlockType.BASE_STONE_OVERWORLD, 
@@ -35,6 +42,7 @@ public class OreGeneration {
 		
 	}
 	
+	//A method to help us generate the ores, it's much easier to call it and define the values instead of typing out a long line of code every time we want to generate a new ore.
 	private static void generateOre(BiomeGenerationSettingsBuilder settings, RuleTest fillerType, BlockState state, int veinSize, int minHeight, int maxHeight, int count) {
 		
 		settings.withFeature(GenerationStage.Decoration.UNDERGROUND_ORES, 
