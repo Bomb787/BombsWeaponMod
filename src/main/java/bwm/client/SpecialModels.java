08a91871df617609c92d7240c32f832e1e052120
Bomb787 <39660651+Bomb787@users.noreply.github.com>
2021-03-17 21:30:06 -0400

    Added comments and more models.

--- src/main/java/bwm/client/SpecialModels.java
+++ src/main/java/bwm/client/SpecialModels.java
@@ -9,15 +9,30 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
 /**
+ * This class allows us to list all of the models that will be used for animations.
  * Author: Original by MrCrayfish, this one by Bomb787
  */
+@EventBusSubscriber(modid = WeaponsMod.MOD_ID, value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
 public enum SpecialModels {
 	
+	/**
+	 * These are all of the models with their file names.
+	 */
 	GLOCK17SLIDE("glock17_slide"),
 	GLOCK17BASE("glock17_base"),
-	GLOCK17MAGAZINE("glock17_magazine");
+	GLOCK17MAGAZINE("glock17_magazine"),
+	GAUSSRIFLEBASE("gauss_rifle_base"),
+	GAUSSRIFLEBARREL("gauss_rifle_barrel"),
+	AK74BASE("ak_74_base"),
+	AK74BOLT("ak_74_bolt"),
+	AK74MAGAZINE("ak_74_magazine"),
+	AK103BASE("ak_103_base"),
+	AK103BOLT("ak_103_bolt"),
+	AK103MAGAZINE("ak_103_magazine"),
+	GEOTEST("geotest");
 	
 	/**
      * The location of an item model in the [MOD_ID]/models/moving/[NAME] folder
